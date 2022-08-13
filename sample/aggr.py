16f19474fe08b5d7ac0d19176bb5a3b2c0ee8be0
moran500 <34867983+moran500@users.noreply.github.com>
2022-08-13 18:57:47 +0200

    merge the files

--- sample/aggr.py
+++ sample/aggr.py
@@ -1,1 +1,22 @@
-print("servus")
+from os import listdir
+from os.path import isfile, join
+import shutil
+class AggrTxtFile():
+
+    def __init__(self, folder:str) -> None:
+        self.merge_files(self.get_files_names_list(folder), folder)
+
+    def merge_files(self, files_list:list, folder_path:str) -> None:
+        for file in files_list:
+            with open(folder_path +"/"+ file,'r') as firstfile, open(folder_path +"/"+"final.txt",'a') as secondfile:
+                secondfile.write("===============TEST=================================")
+                for line in firstfile:
+                    # append content to second file
+                    secondfile.write(line)
+
+    def get_files_names_list(self, folder_path:str) -> list:
+        return  [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+
+
+if __name__ == "__main__":
+    AggrTxtFile("C:/Users/roman/OneDrive/Počítač/toptal_folder/exercises")
